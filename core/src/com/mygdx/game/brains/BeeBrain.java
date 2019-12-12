4ec745e59574ed8b8b8755cfc8ad175f5b86cbe6
Chris-Johanson <caj94@cornell.edu>
2019-12-12 13:58:44 -0500

    ai2

---
+++ core/src/com/mygdx/game/brains/BeeBrain.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.brains;
+
+import io.jenetics.DoubleGene;
+import io.jenetics.Genotype;
+
+public class BeeBrain {
+
+    public BeeBrain(Genotype<DoubleGene> gt){
+
+    }
+
+    double evaluate(){
+
+        return 0;
+    }
+}
